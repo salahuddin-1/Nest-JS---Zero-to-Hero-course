b9e6ef4b6035fbf83de85d2b50c0b1ca57362588
Salahuddin Shaikh <salahuddinshaikh16@gmail.com>
2024-02-12 13:40:47 +0530

    Adding custom Pipe

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,30 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { TaskStatus } from '../task.model';
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  readonly allowedStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE,
+  ];
+
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (this.isStatusValid(value) === false) {
+      throw new BadRequestException(`"${value}" is an invalid status`);
+    }
+
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    // If Item is not found it will return -1, if -1 then item does not exist
+    const idx = this.allowedStatuses.indexOf(status);
+    return idx !== -1;
+  }
+}
