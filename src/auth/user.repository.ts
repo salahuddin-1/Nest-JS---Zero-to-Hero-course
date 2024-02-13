56a07a13f3ff7f584262b70e6581b9ddb340c8a7
Salahuddin Shaikh <salahuddinshaikh16@gmail.com>
2024-02-13 17:46:42 +0530

    Creating auth module and user repo

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,10 @@
+import { EntityManager, Repository } from 'typeorm';
+import { User } from './user.entity';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserRepository extends Repository<User> {
+  constructor(private readonly entityManager: EntityManager) {
+    super(User, entityManager);
+  }
+}
