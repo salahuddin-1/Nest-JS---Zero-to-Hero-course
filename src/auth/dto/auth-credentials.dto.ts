32d845d34e1e2707d2c25aaa3fcbdf673721d78e
Salahuddin Shaikh <salahuddinshaikh16@gmail.com>
2024-02-13 18:18:20 +0530

    Username and pass validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,22 @@
+import {
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+  isString,
+} from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password too weak',
+  })
   password: string;
 }
