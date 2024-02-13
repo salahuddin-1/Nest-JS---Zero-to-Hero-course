f919f738896a8111d366990c5aed9fd2442bc068
Salahuddin Shaikh <salahuddinshaikh16@gmail.com>
2024-02-13 18:04:42 +0530

    Creating sign up request

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userRepository: UserRepository) {}
+
+  signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.signUp(authCredentialsDto);
+  }
+}
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,10 +1,22 @@
 import { EntityManager, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { Injectable } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
   constructor(private readonly entityManager: EntityManager) {
     super(User, entityManager);
+  }
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = new User();
+
+    user.username = username;
+    user.password = password;
+
+    await user.save();
   }
 }
